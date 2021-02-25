d208d2b93d8ccabeefd9b9984451d15b224e6ab2
Kovalev Dmitriy <kacevnik@yandex.ru>
2021-02-25 16:54:15 +0300

    added axios and api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
 import './App.css';
 
 function App() {
+
+  const APIkey = '98e5746d72076df9bc16061c44e98b87';
+  const [city, setSity] = useState('London');
+
+  useEffect(()=>{
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}`).then(res => {
+      console.log(res.data);
+    })
+  },[]);
+
   return (
     <div className="App">
       Test
